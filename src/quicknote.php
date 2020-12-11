a3d76aca5e7505bd2be2b3f8ddb01e4fe145ec5b
Chris Putnam <chris@webfx.com>
2020-12-11 07:43:28 -0500

    initial tool baseline

---
+++ src/quicknote.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * quicknote
+ */
+Class Quicknote extends Console_Abstract
+{
+    const VERSION = "1.0.1";
+
+    // Name of script and directory to store config
+    const SHORTNAME = 'quicknote';
+
+    /**
+     * Callable Methods
+     */
+    protected static $METHODS = [
+        'test',
+    ];
+
+    // Config Variables
+    protected $__test_message = ["Test message - sample config to test new console tool", "string"];
+    public $test_message = "Testing, testing, 0, 1, 10, 11";
+
+    // Update this to your update URL, or remove it to disable updates
+	public $update_version_url = "https://raw.githubusercontent.com/chrisputnam9/pcon/master/tests/dist/test-readme.md";
+
+    protected $___test = [
+        "Sample method to test new console tool",
+        ["Mesage - defaults to value of test_message (config)", "string"],
+    ];
+	public function test($message=null)
+    {
+        $this->prepArg($message, null);
+
+        if (is_null($message))
+        {
+            $this->log('No message specified, using configured test message');
+            $message = $this->test_message;
+            if (empty($message))
+            {
+                $this->error('Failed to load a test message');
+            }
+        }
+
+        $this->output($message);
+    }
+}
+
+// Kick it all off
+Quicknote::run($argv);
+
+// Note: leave this for packaging ?>
