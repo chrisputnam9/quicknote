ee4d4c913917a5dacaccffa0b7484aac35c8a1d1
Chris Putnam <chris@webfx.com>
2020-12-12 15:26:47 -0500

    Map out methods

--- src/quicknote.php
+++ src/quicknote.php
@@ -13,35 +13,110 @@ Class Quicknote extends Console_Abstract
      * Callable Methods
      */
     protected static $METHODS = [
-        'test',
+        'add',
+        'add_as',
+        'add_cl',
+        'add_fx',
+        'add_gh',
+        'add_ml',
+        'add_qn',
+        'add_tx',
     ];
 
-    // Config Variables
-    protected $__test_message = ["Test message - sample config to test new console tool", "string"];
-    public $test_message = "Testing, testing, 0, 1, 10, 11";
-
-    // Update this to your update URL, or remove it to disable updates
-	public $update_version_url = "https://raw.githubusercontent.com/chrisputnam9/pcon/master/tests/dist/test-readme.md";
-
-    protected $___test = [
-        "Sample method to test new console tool",
-        ["Mesage - defaults to value of test_message (config)", "string"],
+    protected $___add = [
+        "Add a new note - with interactive prompt for details"
     ];
-	public function test($message=null)
+	public function add()
     {
-        $this->prepArg($message, null);
+        $types = [
+            "AS - Asana Todo" => "as",
+            "CL - Calendar Event" => "cl",
+            "FX - TFX List" => "fx",
+            "GH - Github Issue" => "gh",
+            "ML - E-mail Message (Email)" => "ml",
+            "QN - Miscellaneous - Quicknotes file" => "qn",
+            "TX - Text Message (TXT, SMS)" => "tx",
+        ];
+        $type_keys = array_keys($types);
 
-        if (is_null($message))
+        $type = $this->select($type_keys, "Select type of note", 5);
+
+        $method = [$this, "add_" . $types[$type]];
+        if (is_callable($method))
         {
-            $this->log('No message specified, using configured test message');
-            $message = $this->test_message;
-            if (empty($message))
-            {
-                $this->error('Failed to load a test message');
-            }
+            call_user_func($method);
         }
+        else
+        {
+            throw new Exception("Invalid method: " . $method[1]);
+        }
+    }
 
-        $this->output($message);
+    protected $___add_as = [
+        "Add a new note - Asana Todo",
+    ];
+	public function add_as()
+    {
+        echo "Add a new note - Asana Todo - Not yet implemented";
+    }
+
+    protected $___add_cl = [
+        "Add a new note - Calendar Event",
+    ];
+	public function add_cl()
+    {
+        echo "Add a new note - Asana Todo - Not yet implemented";
+    }
+
+    protected $___add_fx = [
+        "Add a new note - TFX List",
+    ];
+	public function add_fx()
+    {
+        echo "Add a new note - TFX List -  Not yet implemented";
+
+        $list = $this->select([
+            "TI - Team Initiative",
+            "KS - Knowledge Sharing",
+            "DN - DevNext - Bootcamp, CDT, PP, etc.",
+            "EM - Weekly E-mail",
+            "KZ - Kelly's List",
+        ], "Select list to add to");
+    }
+
+    protected $___add_gh = [
+        "Add a new note - Github Issue",
+    ];
+	public function add_gh()
+    {
+        echo "Add a new note - TFX List - Not yet implemented";
+    }
+
+    protected $___add_ml = [
+        "Add a new note - E-mail Message (Email)",
+    ];
+	public function add_ml()
+    {
+        echo "Add a new note - E-mail Message (Email) - Not yet implemented";
+    }
+
+    protected $___add_qn = [
+        "Add a new note - Miscellaneous - Quicknotes file",
+    ];
+	public function add_qn()
+    {
+        $this->clear();
+        $message = $this->edit();
+        // todo
+        echo "$message";
+    }
+
+    protected $___add_tx = [
+        "Add a new note - Text Message (TXT, SMS)",
+    ];
+	public function add_tx()
+    {
+        echo "Add a new note - Miscellaneous - Quicknotes file - Not yet implemented";
     }
 }
 
